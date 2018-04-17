ed4f6e3489aedf92bc41d47376466574064d1a96
wang <wanglei4work@gmail.com>
2018-04-17 17:27:35 +0800

    1.continue_ex1

--- src/control_continue.java
+++ src/control_continue.java
@@ -1,8 +1,11 @@
 public class control_continue {
     public static void main(String[] args) {
-        System.out.println();
+        // 如果是双数，后面的代码不执行，直接进入下一次循环
         for (int i = 0; i < 10; i++) {
-            if
+            if(i%2 == 0){
+                continue;
+            }
+            System.out.println(i);
         }
     }
 }
