2e4ba119b716c2e84693378651db7d7b3307fad2
wang <wanglei4work@gmail.com>
2018-04-18 09:27:27 +0800

    V1.0.0 1.上传control_continue_ex2 忽略倍数

--- src/control_continue.java
+++ src/control_continue.java
@@ -1,11 +1,19 @@
 public class control_continue {
     public static void main(String[] args) {
         // 如果是双数，后面的代码不执行，直接进入下一次循环
-        for (int i = 0; i < 10; i++) {
-            if(i%2 == 0){
+//        for (int i = 0; i < 10; i++) {
+//            if(i%2 == 0){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+        // 打印 1-100之前的数，如果这个数，要么是3，要么5的倍数，就忽略掉
+        for (int j = 1; j < 100; j++) {
+            if ((j%3 == 0) || (j%5 == 0)){
                 continue;
             }
-            System.out.println(i);
+            System.out.println(j);
         }
     }
 }
